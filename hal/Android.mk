956f102ce7904f3233d2be20bee28c014e714048
Fazil Sheik <fazil.sheik96@gmail.com>
2025-01-30 19:57:41 +0530

    hal: Convert primary HAL to blueprint

--- hal/Android.mk
+++ hal/Android.mk
@@ -1,4 +1,3 @@
-ifneq ($(TARGET_PROVIDES_AUDIO_HAL),true)
 ifneq ($(AUDIO_USE_STUB_HAL), true)
 ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
 LOCAL_PATH := $(call my-dir)
@@ -65,12 +64,6 @@ endif
 ifneq ($(filter qcs605,$(TARGET_BOARD_PLATFORM)),)
   LOCAL_CFLAGS := -DPLATFORM_QCS605
 endif
-ifneq ($(filter msmnile sdmshrike,$(TARGET_BOARD_PLATFORM)),)
-  LOCAL_CFLAGS := -DPLATFORM_MSMNILE
-  LOCAL_CFLAGS += -DMAX_TARGET_SPECIFIC_CHANNEL_CNT="4"
-  LOCAL_CFLAGS += -DINCALL_MUSIC_ENABLED
-  LOCAL_CFLAGS += -DINCALL_STEREO_CAPTURE_ENABLED
-endif
 ifneq ($(filter kona lahaina,$(TARGET_BOARD_PLATFORM)),)
   LOCAL_CFLAGS := -DPLATFORM_KONA
   LOCAL_CFLAGS += -DMAX_TARGET_SPECIFIC_CHANNEL_CNT="4"
@@ -118,76 +111,11 @@ ifneq ($(filter msm8953,$(TARGET_BOARD_PLATFORM)),)
 endif
 endif
 
-ifeq ($(TARGET_BOARD_AUTO),true)
-  LOCAL_CFLAGS += -DPLATFORM_AUTO
-endif
-
-LOCAL_CFLAGS += -Wno-macro-redefined
-
-LOCAL_HEADER_LIBRARIES := libhardware_headers
-
-LOCAL_SRC_FILES := \
-    audio_hw.c \
-    acdb.c \
-    platform_info.c \
-    $(AUDIO_PLATFORM)/platform.c \
-    voice.c
-
-LOCAL_SRC_FILES += audio_extn/audio_extn.c \
-                   audio_extn/audio_hidl.cpp \
-                   audio_extn/compress_in.c \
-                   audio_extn/fm.c \
-                   audio_extn/keep_alive.c \
-                   audio_extn/source_track.c \
-                   audio_extn/usb.c \
-                   audio_extn/utils.c \
-                   audio_extn/device_utils.c \
-                   voice_extn/compress_voip.c \
-                   voice_extn/voice_extn.c
-
-LOCAL_SHARED_LIBRARIES := \
-    libbase \
-    liblog \
-    libcutils \
-    libtinyalsa \
-    libtinycompress \
-    libaudioroute \
-    libdl \
-    libaudioutils \
-    libexpat \
-    libhidlbase \
-    libprocessgroup \
-    libutils
-
-LOCAL_C_INCLUDES += \
-    external/tinyalsa/include \
-    external/tinycompress/include \
-    system/media/audio_utils/include \
-    external/expat/lib \
-    vendor/qcom/opensource/core-utils/fwk-detect \
-    $(call include-path-for, audio-route) \
-    $(call include-path-for, audio-effects) \
-    $(LOCAL_PATH)/$(AUDIO_PLATFORM) \
-    $(LOCAL_PATH)/audio_extn \
-    $(LOCAL_PATH)/voice_extn
-
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/audio
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-
 # Hardware specific feature
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DLKM)),true)
   LOCAL_HEADER_LIBRARIES += audio_kernel_headers
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
 endif
-
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)),true)
-  LOCAL_CFLAGS += -DENABLE_EXTENDED_COMPRESS_FORMAT
-endif
-
-LOCAL_CFLAGS += -DUSE_VENDOR_EXTN
 
 # Legacy feature
 ifdef MULTIPLE_HW_VARIANTS_ENABLED
@@ -248,14 +176,6 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_LISTEN)),true)
     LOCAL_SRC_FILES += audio_extn/listen.c
 endif
 
-# Hardware specific feature
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-        LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-        LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/audio
-        LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
-        LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-endif
-
 # Legacy feature
 ifeq ($(strip $(AUDIO_FEATURE_SUPPORTED_EXTERNAL_BT)),true)
   LOCAL_CFLAGS += -DEXTERNAL_BT_SUPPORTED
@@ -288,17 +208,6 @@ ifeq ($(strip $(BOARD_SUPPORTS_SOUND_TRIGGER_HAL)),true)
     ST_FEATURE_ENABLE := true
 endif
 
-# Hardware specific feature
-ifeq ($(ST_FEATURE_ENABLE), true)
-    LOCAL_CFLAGS += -DSOUND_TRIGGER_ENABLED
-    LOCAL_CFLAGS += -DSOUND_TRIGGER_PLATFORM_NAME=$(TARGET_BOARD_PLATFORM)
-    LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/sound_trigger
-    LOCAL_SRC_FILES += audio_extn/soundtrigger.c
-ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-    LOCAL_HEADER_LIBRARIES += sound_trigger.primary_headers
-endif
-endif
-
 # Legacy feature
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AUXPCM_BT)),true)
     LOCAL_CFLAGS += -DAUXPCM_BT_ENABLED
@@ -312,34 +221,16 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PM_SUPPORT)),true)
     LOCAL_SHARED_LIBRARIES += libperipheral_client
 endif
 
-
-# Hardare specific featre
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_GEF_SUPPORT)),true)
-    LOCAL_CFLAGS += -DAUDIO_GENERIC_EFFECT_FRAMEWORK_ENABLED
-    LOCAL_SRC_FILES += audio_extn/gef.c
-endif
-
 # Hardware specific feature
 ifeq ($(strip $($AUDIO_FEATURE_ADSP_HDLR_ENABLED)),true)
     LOCAL_CFLAGS += -DAUDIO_EXTN_ADSP_HDLR_ENABLED
     LOCAL_SRC_FILES += audio_extn/adsp_hdlr.c
 endif
 
-
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DYNAMIC_LOG)), true)
     LOCAL_CFLAGS += -DDYNAMIC_LOG_ENABLED
     LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-log-utils
     LOCAL_SHARED_LIBRARIES += libaudio_log_utils
-endif
-
-# Hardware specific feature
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_INSTANCE_ID)), true)
-    LOCAL_CFLAGS += -DINSTANCE_ID_ENABLED
-endif
-
-# Kernel specific feature
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_GKI)), true)
-    LOCAL_CFLAGS += -DAUDIO_GKI_ENABLED
 endif
 
 # Legacy feature
@@ -360,61 +251,15 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AHAL_EXT)),true)
     LOCAL_SHARED_LIBRARIES += vendor.qti.hardware.audiohalext@1.0
 endif
 
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER)),true)
-    LOCAL_CFLAGS += -DEXT_AMPLIFIER_ENABLED
-    LOCAL_SRC_FILES += audio_extn/audio_amplifier.c
-    LOCAL_SHARED_LIBRARIES += libhardware
-endif
-
-LOCAL_CFLAGS += -D_GNU_SOURCE
-LOCAL_CFLAGS += -Wall -Werror
-
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_GCOV)),true)
     LOCAL_CFLAGS += --coverage -fprofile-arcs -ftest-coverage
     LOCAL_CPPFLAGS += --coverage -fprofile-arcs -ftest-coverage
     LOCAL_STATIC_LIBRARIES += libprofile_rt
 endif
 
-LOCAL_SHARED_LIBRARIES += libbase libhidlbase libutils android.hardware.power@1.2 liblog
-
-LOCAL_SHARED_LIBRARIES += android.hardware.power-V1-ndk
-LOCAL_SHARED_LIBRARIES += libbinder_ndk
-
-LOCAL_SRC_FILES += audio_perf.cpp
-
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FM_TUNER_EXT)),true)
     LOCAL_CFLAGS += -DFM_TUNER_EXT_ENABLED
 endif
 
-LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
-
-LOCAL_MODULE_RELATIVE_PATH := hw
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_MODULE_OWNER := qti
-
-LOCAL_VENDOR_MODULE := true
-
-ifneq ($(filter kona lahaina holi,$(TARGET_BOARD_PLATFORM)),)
-LOCAL_SANITIZE := integer_overflow
-endif
-
-LOCAL_CFLAGS += -Wno-unused-variable
-LOCAL_CFLAGS += -Wno-sign-compare
-LOCAL_CFLAGS += -Wno-unused-parameter
-LOCAL_CFLAGS += -Wno-unused-label
-LOCAL_CFLAGS += -Wno-gnu-designator
-LOCAL_CFLAGS += -Wno-typedef-redefinition
-LOCAL_CFLAGS += -Wno-shorten-64-to-32
-LOCAL_CFLAGS += -Wno-tautological-compare
-LOCAL_CFLAGS += -Wno-unused-function
-LOCAL_CFLAGS += -Wno-unused-local-typedef
-
-include $(BUILD_SHARED_LIBRARY)
-
-endif
 endif
 endif
